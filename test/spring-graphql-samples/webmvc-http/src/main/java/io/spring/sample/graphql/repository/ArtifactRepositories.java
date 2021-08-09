a29a5ff977f8521f800b5c0672f1ae91accdf104
Oscar(오정우) <jungwoo.oh@carrotins.com>
2021-08-09 09:55:10 +0900

    테스트 프로젝트 수정

--- test/spring-graphql-samples/webmvc-http/src/main/java/io/spring/sample/graphql/repository/ArtifactRepositories.java
+++
@@ -1,9 +0,0 @@
-package io.spring.sample.graphql.repository;
-
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ArtifactRepositories extends
-		CrudRepository<ArtifactRepository, String>, QuerydslPredicateExecutor<ArtifactRepository> {
-
-}
