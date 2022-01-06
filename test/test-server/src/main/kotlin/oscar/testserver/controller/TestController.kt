95dde5d34c9a4fe21f138c13414947ec628bdd2d
Oscar(오정우) <jungwoo.oh@carrotins.com>
2022-01-06 17:34:17 +0900

    fix test code

--- test/test-server/src/main/kotlin/oscar/testserver/controller/TestController.kt
+++ test/test-server/src/main/kotlin/oscar/testserver/controller/TestController.kt
@@ -1,16 +1,14 @@
 package oscar.testserver.controller
 
 import oscar.testserver.service.TestService
-import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
 import java.util.concurrent.Callable
 
 
-@Controller
+@RestController
 class TestController(val testService: TestService) {
 
-    @GetMapping("index")
+    @GetMapping("/index")
     fun index(): Callable<String> {
 //        val bar = testService.bar()
 //        return ResponseEntity.ok(bar)
